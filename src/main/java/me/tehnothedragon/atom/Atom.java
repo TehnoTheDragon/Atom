e881025b5990d69322d8aa4c6e41d91eeecf630b
TehnoTheDragon <tehnothedragon@gmail.com>
2024-05-06 21:35:21 +0300

    init

---
+++ src/main/java/me/tehnothedragon/atom/Atom.java
@@ -0,0 +1,10 @@
+package me.tehnothedragon.atom;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Atom implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
